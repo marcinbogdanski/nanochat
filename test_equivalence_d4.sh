d4fda791650fc4775e994b9dfed686042047a217
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-02-08 00:32:33 +0000

    Add test_equivalence_d4, confirm md5 model weights, v06

---
+++ test_equivalence_d4.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CUBLAS_WORKSPACE_CONFIG=:4096:8 OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=4 -m scripts.base_train \
+  -- --depth=4 --device_batch_size=8 \
+  --eval_every=10 --core_metric_every=10 --sample_every=10 --save_every=10 --num_iterations=100
+# --run=d4k
