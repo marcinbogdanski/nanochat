b0caed0e8eea822296525986988a68e4e32a5e9f
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2026-08-08 23:10:45 +0000

    Fix test_equivalence_sft_d4.sh

--- test_equivalence_sft_d4.sh
+++ test_equivalence_sft_d4.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 CUDA_VISIBLE_DEVICES=2,3 CUBLAS_WORKSPACE_CONFIG=:4096:8 OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=2 -m scripts.chat_sft \
-  -- --model-tag=d4 \
-  --eval-every=10 --eval-tokens=524288 --chatcore-every=-1 --num-iterations=64
+  -- --model-tag=d4 --total-batch-size=32768 \
+  --eval-every=10 --eval-tokens=524288 --chatcore-every=-1 --num-iterations=5
 
